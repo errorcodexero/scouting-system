7c83a17a9c5ca1442158c7bbc264b9ce2115ddfc
Nick Burrell <nicholas@burrellhome.com>
2016-10-28 22:15:27 -0700

    Added more structure, lots of progress on XML for Scouting Activity

---
+++ XeroScouterCollect/app/src/main/java/wilsonvillerobotics/com/xeroscoutercollect/CSVConverter.java
@@ -0,0 +1,11 @@
+package wilsonvillerobotics.com.xeroscoutercollect;
+
+/**
+ * Created by nick on 10/28/16.
+ */
+public interface CSVConverter{
+
+    String DataToCSV();
+
+
+}
--- XeroScouterCollect/app/src/main/java/wilsonvillerobotics/com/xeroscoutercollect/SyncActivity.java
+++ XeroScouterCollect/app/src/main/java/wilsonvillerobotics/com/xeroscoutercollect/SyncActivity.java
@@ -3,11 +3,11 @@ package wilsonvillerobotics.com.xeroscoutercollect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Scouting extends AppCompatActivity {
+public class SyncActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scouting);
+        setContentView(R.layout.activity_sync);
     }
 }
