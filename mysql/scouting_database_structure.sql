df19d62c5da9c0b5c28d0145fb98785a2aff4aaf
pb-r900nh4x-w81\cvbriggler <cvbriggler@gmail.com>
2016-10-29 10:14:39 -0700

    This is the master copy of the scouting database structure. It will contain table definitions, view definitions, idexes, foreign keys, stored procedures, and any oher database object. It does not include data. This script can be used to create a scouting schema in a MySQL 5.7 database.

---
+++ mysql/scouting_database_structure.sql
@@ -0,0 +1,178 @@
+CREATE DATABASE  IF NOT EXISTS `scouting` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `scouting`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: scouting
+-- ------------------------------------------------------
+-- Server version	5.7.15-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `action_type`
+--
+
+DROP TABLE IF EXISTS `action_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `action_type` (
+  `action_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT 'No Name',
+  `description` varchar(2000) DEFAULT 'No description',
+  `match_phase` varchar(45) DEFAULT NULL,
+  `points` int(11) DEFAULT '0',
+  `opponent_points` int(11) DEFAULT '0',
+  `qual_points` int(11) DEFAULT '0',
+  `foul_points` int(11) DEFAULT '0',
+  `coop_flag` char(1) DEFAULT 'N',
+  `category` varchar(255) DEFAULT 'Uncategorized',
+  PRIMARY KEY (`action_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event` (
+  `event_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tba_event_key` varchar(45) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `short_name` varchar(255) DEFAULT NULL,
+  `event_type` varchar(255) DEFAULT NULL,
+  `event_district` varchar(255) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `week` int(11) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `tba_event_code` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`event_id`),
+  KEY `idx_tba_event_key` (`tba_event_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `match`
+--
+
+DROP TABLE IF EXISTS `match`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `match` (
+  `match_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_id` int(11) NOT NULL,
+  `tba_match_key` varchar(255) DEFAULT NULL,
+  `comp_level` varchar(45) DEFAULT NULL COMMENT 'This is a code that indicates whether this is a qualifier, quarter final, semi final, or final match.',
+  `set_number` varchar(45) DEFAULT NULL,
+  `match_number` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL COMMENT 'Status of match. Not started, complete, cancelled, delayed, etc.\n',
+  `red_auto_score` int(11) DEFAULT NULL,
+  `red_teleop_score` int(11) DEFAULT NULL,
+  `red_total_score` int(11) DEFAULT NULL,
+  `red_qp` int(11) DEFAULT NULL,
+  `red_foul_points` int(11) DEFAULT NULL,
+  `blue_auto_score` int(11) DEFAULT NULL,
+  `blue_teleop_score` int(11) DEFAULT NULL,
+  `blue_total_score` int(11) DEFAULT NULL,
+  `blue_qp` int(11) DEFAULT NULL,
+  `blue_foul_points` int(11) DEFAULT NULL,
+  `winner` varchar(45) DEFAULT NULL,
+  `drive_team_comments` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`match_id`),
+  KEY `fk_match_event_idx` (`event_id`),
+  CONSTRAINT `fk_match_event` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team` (
+  `team_id` int(11) NOT NULL COMMENT 'The FRC team number. Example: 1425, 360, 4488',
+  `tba_team_key` varchar(45) DEFAULT NULL COMMENT 'This is the key used by The Blue Alliance website. This key is used when retreiving team data from the TBA API.',
+  `long_name` varchar(255) DEFAULT NULL COMMENT 'Team name. Example: Error Code Xero, Flaming Chickens',
+  `name` varchar(255) DEFAULT NULL,
+  `logo_file_location` varchar(2000) DEFAULT NULL COMMENT 'The file location of a logo file. This could be a file path or a url.',
+  `city` varchar(255) DEFAULT NULL,
+  `state_code` varchar(45) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `motto` varchar(2000) DEFAULT NULL,
+  `rookie_year` int(11) DEFAULT NULL,
+  `robot_name` varchar(255) DEFAULT NULL,
+  `robot_picture_file_location` varchar(2000) DEFAULT NULL COMMENT 'File path or url to robot picture',
+  `robot_drive_type` varchar(45) DEFAULT NULL,
+  `robot_wheel_count` int(11) DEFAULT NULL,
+  `robot_drive_motor_count` int(11) DEFAULT NULL,
+  `robot_description` varchar(2000) DEFAULT NULL,
+  `pit_scout_comments` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`team_id`),
+  KEY `idx_tba_team_key` (`tba_team_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This table contains FRC team data for a competition year. It includes information about their robot.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `team_match`
+--
+
+DROP TABLE IF EXISTS `team_match`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team_match` (
+  `team_match_id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_id` int(11) NOT NULL,
+  `match_id` int(11) NOT NULL,
+  `alliance` varchar(45) DEFAULT NULL,
+  `position` int(11) DEFAULT NULL,
+  `team_matchcol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`team_match_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `team_match_action`
+--
+
+DROP TABLE IF EXISTS `team_match_action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team_match_action` (
+  `team_match_action_id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_match_id` int(11) NOT NULL,
+  `action_type_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT '1',
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `object_count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`team_match_action_id`),
+  KEY `fk_tma_team_match_idx` (`team_match_id`),
+  KEY `fk_tma_action_type_idx` (`action_type_id`),
+  CONSTRAINT `fk_tma_action_type` FOREIGN KEY (`action_type_id`) REFERENCES `action_type` (`action_type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tma_team_match` FOREIGN KEY (`team_match_id`) REFERENCES `team_match` (`team_match_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-10-28 11:43:34
