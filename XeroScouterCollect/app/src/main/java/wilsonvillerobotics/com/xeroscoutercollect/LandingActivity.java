b38bf593564e453125511c13ecb59d88b11873a5
Luke Puppo <lukepuppo@gmail.com>
2016-11-05 11:59:14 -0700

    Added Landing Page
    Needs menu bar

---
+++ XeroScouterCollect/app/src/main/java/wilsonvillerobotics/com/xeroscoutercollect/LandingActivity.java
@@ -0,0 +1,16 @@
+package wilsonvillerobotics.com.xeroscoutercollect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Luke on 11/5/2016.
+ */
+public class LandingActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
