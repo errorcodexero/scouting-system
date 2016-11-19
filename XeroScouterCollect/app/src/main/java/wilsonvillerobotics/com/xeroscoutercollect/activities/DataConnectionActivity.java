f5d8a05660206b3a295994729e22b48397d14165
Luke Puppo <lukepuppo@gmail.com>
2016-11-19 15:05:35 -0800

    Able to send bluetooth documents

---
+++ XeroScouterCollect/app/src/main/java/wilsonvillerobotics/com/xeroscoutercollect/activities/DataConnectionActivity.java
@@ -0,0 +1,37 @@
+package wilsonvillerobotics.com.xeroscoutercollect.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import wilsonvillerobotics.com.xeroscoutercollect.R;
+import wilsonvillerobotics.com.xeroscoutercollect.connection.DataConnectionManager;
+
+/**
+ * Created by Luke on 11/19/2016.
+ */
+
+public class DataConnectionActivity extends Activity implements View.OnClickListener{
+
+    private DataConnectionManager dataConnectionManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_connection);
+        dataConnectionManager = new DataConnectionManager(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_enable_bluetooth){
+            dataConnectionManager.setConnectionToBluetooth();
+            Toast.makeText(this,"Set connection to Bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        if(view.getId() == R.id.btn_send_file){
+            dataConnectionManager.sendFile();
+        }
+    }
+}
---
+++ XeroScouterCollect/app/src/main/java/wilsonvillerobotics/com/xeroscoutercollect/connection/ConnectionType.java
@@ -0,0 +1,27 @@
+package wilsonvillerobotics.com.xeroscoutercollect.connection;
+
+/**
+ * Created by Luke on 11/19/2016.
+ */
+
+public class ConnectionType {
+
+
+    public ConnectionType(){
+
+    }
+
+    public boolean sendDataFile(){
+        return true;
+    }
+
+    public boolean receiveDataFile(){
+        return true;
+    }
+
+    public boolean initAdapter(){
+        return true;
+    }
+
+
+}
