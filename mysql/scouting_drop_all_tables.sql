c8e1c98bf464a5caf16d8fc55019174015e45ecb
Tom Sommerville <tom.sommerville@gmail.com>
2017-01-30 19:30:53 -0800

    Updated Drop Table script

--- mysql/scouting_drop_all_tables.sql
+++ mysql/scouting_drop_all_tables.sql
@@ -1,8 +1,9 @@
-DROP TABLE IF EXISTS `scouting_test`.team_match_action;
-DROP TABLE IF EXISTS `scouting_test`.team_match;
-DROP TABLE IF EXISTS `scouting_test`.team_event;
-DROP TABLE IF EXISTS `scouting_test`.team_old;
-DROP TABLE IF EXISTS `scouting_test`.match;
-DROP TABLE IF EXISTS `scouting_test`.team;
-DROP TABLE IF EXISTS `scouting_test`.action_type;
-DROP TABLE IF EXISTS `scouting_test`.event;
+use scouting_test;
+DROP TABLE IF EXISTS team_match_action;
+DROP TABLE IF EXISTS team_match;
+DROP TABLE IF EXISTS team_event;
+DROP TABLE IF EXISTS team_old;
+DROP TABLE IF EXISTS `match`;
+DROP TABLE IF EXISTS team;
+DROP TABLE IF EXISTS action_type;
+DROP TABLE IF EXISTS `event`;
