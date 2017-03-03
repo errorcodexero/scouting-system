f7d31fef47132cbde7628254fa23e0be32ce7870
Nick Burrell <nicholas@burrellhome.com>
2017-03-02 20:49:22 -0800

    Undid my spinners for now

--- XeroScouterCollect/app/src/main/java/wilsonvillerobotics/com/xeroscoutercollect/activities/EventSetupActivity.java
+++ XeroScouterCollect/app/src/main/java/wilsonvillerobotics/com/xeroscoutercollect/activities/EventSetupActivity.java
@@ -1,34 +1,68 @@
 package wilsonvillerobotics.com.xeroscoutercollect.activities;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import wilsonvillerobotics.com.xeroscoutercollect.R;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import wilsonvillerobotics.com.xeroscoutercollect.R;
+import wilsonvillerobotics.com.xeroscoutercollect.contracts.EventContract;
+import wilsonvillerobotics.com.xeroscoutercollect.database.DatabaseHelper;
+
+public class EventSetupActivity extends Activity implements View.OnClickListener{
 
     String strTabletID, strEventName;
     boolean idEmpty, nameEmpty, idValid, nameValid;
 
+    ArrayList<String> eventList = new ArrayList<>();
+    ArrayAdapter<String> eventArrayAdapter;
+    HashMap<String, Integer> eventIdMap = new HashMap<>();
+    DatabaseHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_event_setup);
         Intent intent;
         String pref_default = getString(R.string.default_pref_value);
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        //SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences sharedPreferences = this.getPreferences(this.MODE_PRIVATE);
         strTabletID = sharedPreferences.getString(getString(R.string.tablet_id_pref), pref_default);
         strEventName = sharedPreferences.getString(getString(R.string.event_name_pref), pref_default);
+
+        dbHelper = DatabaseHelper.getInstance(this);
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        String queryString = "SELECT * FROM event;";
+
+        Cursor cursor = db.rawQuery(queryString, null);
+        eventList.add("Select An Event");
+        while(cursor.moveToNext()) {
+            eventList.add(cursor.getString(cursor.getColumnIndex(EventContract.EventEntry.COLUMN_NAME_EVENT_NAME)));
+            eventIdMap.put(cursor.getString(cursor.getColumnIndex(EventContract.EventEntry.COLUMN_NAME_EVENT_NAME)), cursor.getInt(cursor.getColumnIndex("_id")));
+        }
+
+        eventArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, eventList);
+
+        eventArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner eventSpinner = (Spinner) findViewById(R.id.event_setup_spinner);
+
+        eventSpinner.setAdapter(eventArrayAdapter);
 
         idValid = nameValid = false;
 
@@ -36,82 +70,39 @@ public class MainActivity extends Activity implements View.OnClickListener {
         nameEmpty = (strEventName.equalsIgnoreCase(pref_default));
 
         if(idEmpty || nameEmpty) {
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_event_setup);
             if(idEmpty && nameEmpty) {
                 Toast.makeText(this, "Tablet ID and Event Name need to be set!", Toast.LENGTH_LONG).show();
             } else if(idEmpty) {
-                EditText txtName = (EditText)findViewById(R.id.txt_main_event_name);
-                if(txtName != null) {
-                    txtName.setVisibility(View.GONE);
-                }
                 TextView lblName = (TextView)findViewById(R.id.lbl_main_event_name);
                 if(lblName != null) {
                     lblName.setVisibility(View.GONE);
                 }
                 Toast.makeText(this, "Tablet ID needs to be set!", Toast.LENGTH_LONG).show();
-            } else if(nameEmpty) {
-                Toast.makeText(this, "Event Name needs to be set!", Toast.LENGTH_LONG).show();
-                EditText txtID = (EditText)findViewById(R.id.txt_main_tablet_id);
-                if(txtID != null) {
-                    txtID.setVisibility(View.GONE);
-                }
-                TextView lblID = (TextView)findViewById(R.id.lbl_main_tablet_id);
-                if(lblID != null) {
-                    lblID.setVisibility(View.GONE);
-                }
             }
         } else {
-            startLandingActivity();
+            //startLandingActivity();
         }
     }
 
     @Override
     public void onClick(View view) {
         if(view.getId() == R.id.btnSubmit) {
-            validatePreferences();
             savePreferences();
             startLandingActivity();
         }
     }
 
-    private void validatePreferences()
-    {
-        if(idEmpty) {
-            EditText txtID = (EditText)findViewById(R.id.txt_main_tablet_id);
-            if(txtID != null) {
-                String strID = txtID.getText().toString();
-                if(!(strID.compareTo(getString(R.string.default_pref_value)) == 0 || strID.isEmpty())) {
-                    strTabletID = strID;
-                    idValid = true;
-                }
-            }
-        }
-
-        if(nameEmpty) {
-            EditText txtName = (EditText)findViewById(R.id.txt_main_event_name);
-            if(txtName != null) {
-                String strName = txtName.getText().toString();
-                if(!(strName.compareTo(getString(R.string.default_pref_value)) == 0 || strName.isEmpty())) {
-                    strEventName = strName;
-                    nameValid = true;
-                }
-            }
-        }
-    }
 
     private void savePreferences() {
+        Spinner spinner = (Spinner) findViewById(R.id.event_setup_spinner);
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        if(idValid) {
-            editor.putString(getString(R.string.tablet_id_pref), strTabletID);
-        }
-
-        if(nameValid) {
-            editor.putString(getString(R.string.event_name_pref), strEventName);
-        }
+        int tempInt = eventIdMap.get(spinner.getSelectedItem());
+        editor.putInt("event_id", eventIdMap.get(spinner.getSelectedItem()));
 
         editor.commit();
+        Toast.makeText(this, String.valueOf(sharedPreferences.getInt("event_id", 0)), Toast.LENGTH_SHORT).show();
     }
     private void startLandingActivity() {
         Intent intent = new Intent(this, LandingActivity.class);
--- XeroScouterCollect/app/src/main/java/wilsonvillerobotics/com/xeroscoutercollect/activities/MainActivity_new.java
+++ XeroScouterCollect/app/src/main/java/wilsonvillerobotics/com/xeroscoutercollect/activities/MainActivity_new.java
@@ -4,31 +4,67 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import wilsonvillerobotics.com.xeroscoutercollect.R;
+import wilsonvillerobotics.com.xeroscoutercollect.contracts.EventContract;
+import wilsonvillerobotics.com.xeroscoutercollect.database.DatabaseHelper;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-
+public class MainActivity_new extends Activity implements View.OnClickListener {
+/*
     String strTabletID, strEventName;
     boolean idEmpty, nameEmpty, idValid, nameValid;
+
+    ArrayList<String> eventList = new ArrayList<>();
+    ArrayAdapter<String> eventArrayAdapter;
+    DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         Intent intent;
         String pref_default = getString(R.string.default_pref_value);
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         strTabletID = sharedPreferences.getString(getString(R.string.tablet_id_pref), pref_default);
         strEventName = sharedPreferences.getString(getString(R.string.event_name_pref), pref_default);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("resetEventId", true);
+        editor.commit();
+
+
+        dbHelper = DatabaseHelper.getInstance(this);
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        String queryString = "SELECT * FROM event;";
+
+        Cursor cursor = db.rawQuery(queryString, null);
+        eventList.add("Select A Team");
+        while(cursor.moveToNext()) {
+            eventList.add(cursor.getString(cursor.getColumnIndex(EventContract.EventEntry.COLUMN_NAME_EVENT_NAME)));
+        }
+
+        eventArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, eventList);
+
+        Spinner eventSpinner = (Spinner) findViewById(R.id.event_spinner);
+
+        eventSpinner.setAdapter(eventArrayAdapter);
 
         idValid = nameValid = false;
 
@@ -40,10 +76,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
             if(idEmpty && nameEmpty) {
                 Toast.makeText(this, "Tablet ID and Event Name need to be set!", Toast.LENGTH_LONG).show();
             } else if(idEmpty) {
-                EditText txtName = (EditText)findViewById(R.id.txt_main_event_name);
-                if(txtName != null) {
-                    txtName.setVisibility(View.GONE);
-                }
                 TextView lblName = (TextView)findViewById(R.id.lbl_main_event_name);
                 if(lblName != null) {
                     lblName.setVisibility(View.GONE);
@@ -86,31 +118,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 }
             }
         }
-
-        if(nameEmpty) {
-            EditText txtName = (EditText)findViewById(R.id.txt_main_event_name);
-            if(txtName != null) {
-                String strName = txtName.getText().toString();
-                if(!(strName.compareTo(getString(R.string.default_pref_value)) == 0 || strName.isEmpty())) {
-                    strEventName = strName;
-                    nameValid = true;
-                }
-            }
-        }
     }
 
     private void savePreferences() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("event_id", 1);
+        editor.commit();private
+        editor.putString(getString(R.string.event_name_pref), "0");
 
         if(idValid) {
             editor.putString(getString(R.string.tablet_id_pref), strTabletID);
         }
-
-        if(nameValid) {
-            editor.putString(getString(R.string.event_name_pref), strEventName);
-        }
-
         editor.commit();
     }
     private void startLandingActivity() {
@@ -119,5 +138,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         intent.putExtra(getString(R.string.tablet_id_intent_key), strTabletID);
         startActivity(intent);
         finish();
+    }*/
+
+    public void onClick(View v) {
+
     }
 }
